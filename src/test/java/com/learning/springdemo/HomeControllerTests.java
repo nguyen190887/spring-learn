89cc0a8d9dc44086ecdca05ae20d45de415f3726
nguyen190887 <nguyen190887@gmail.com>
2020-03-02 06:42:34 +0700

    Show errors; add MvcWebConfigurer

--- src/test/java/com/learning/springdemo/HomeControllerTests.java
+++ src/test/java/com/learning/springdemo/HomeControllerTests.java
@@ -1,10 +1,8 @@
 package com.learning.springdemo;
 
-import com.learning.springdemo.web.HomeController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -13,7 +11,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(HomeController.class)
 public class HomeControllerTests {
 
     @Autowired
