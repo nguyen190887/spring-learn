a453baec7d0f2ace078d9ef5c0fb99252be7a5ad
Tri-Nguyen <tri.nguyen@coxautoinc.com>
2020-03-18 07:08:56 +0700

    Add basic security

---
+++ src/main/java/com/learning/springdemo/security/SecurityConfig.java
@@ -0,0 +1,14 @@
+package com.learning.springdemo.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+    }
+}
