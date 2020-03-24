6b0fdbab48875442a9d2d7c8705fe8c7dc6924a8
nguyen190887 <nguyen190887@gmail.com>
2020-03-24 07:31:39 +0700

    Add registration/login pages

---
+++ src/main/java/com/learning/springdemo/security/RegistrationForm.java
@@ -0,0 +1,30 @@
+package com.learning.springdemo.security;
+
+import com.learning.springdemo.User;
+import lombok.Data;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Data
+public class RegistrationForm {
+    private String username;
+    private String password;
+    private String fullname;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+    private String phone;
+
+    public User toUser(PasswordEncoder passwordEncoder) {
+        return new User(
+                username,
+                passwordEncoder.encode(password),
+                fullname,
+                street,
+                city,
+                state,
+                zip,
+                phone
+        );
+    }
+}
