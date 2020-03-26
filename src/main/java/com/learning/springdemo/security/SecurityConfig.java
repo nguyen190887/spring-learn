add5f634d2027d33af08fae06e930523506a280e
Tri-Nguyen <tri.nguyen@coxautoinc.com>
2020-03-26 07:12:07 +0700

    Add security principle; add configuration

--- src/main/java/com/learning/springdemo/security/SecurityConfig.java
+++ src/main/java/com/learning/springdemo/security/SecurityConfig.java
@@ -8,10 +8,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
-import sun.security.util.Password;
 
 import javax.sql.DataSource;
 
--- src/main/java/com/learning/springdemo/web/OrderController.java
+++ src/main/java/com/learning/springdemo/web/OrderController.java
@@ -1,8 +1,11 @@
 package com.learning.springdemo.web;
 
 import com.learning.springdemo.Order;
+import com.learning.springdemo.User;
 import com.learning.springdemo.data.OrderRepository;
+import com.learning.springdemo.data.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
+import java.security.Principal;
 
 @Slf4j
 @Controller
@@ -19,6 +23,7 @@ import javax.validation.Valid;
 public class OrderController {
 
     private final OrderRepository orderRepo;
+    private final UserRepository userRepo;
 
     public OrderController(OrderRepository ordeRepo) {
         this.orderRepo = ordeRepo;
@@ -30,10 +35,14 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(@Valid Order order, Errors errors, SessionStatus sessionStatus) {
+    public String processOrder(@Valid Order order, Errors errors,
+                               SessionStatus sessionStatus,
+                               @AuthenticationPrincipal User user) {
         if (errors.hasErrors()) {
             return "orderForm";
         }
+
+        order.setUser(user);
 
         orderRepo.save(order);
         sessionStatus.setComplete();
