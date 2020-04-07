7da7bee9ac38c95d9b6b7ba693185cfce6a0ef17
Tri-Nguyen <tri.nguyen@coxautoinc.com>
2020-04-08 06:11:59 +0700

    Add more configuration

--- src/main/java/com/learning/springdemo/data/OrderRepository.java
+++ src/main/java/com/learning/springdemo/data/OrderRepository.java
@@ -1,8 +1,13 @@
 package com.learning.springdemo.data;
 
 import com.learning.springdemo.Order;
+import com.learning.springdemo.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface OrderRepository extends CrudRepository<Order, Long> {
     Order save(Order order);
+    List<Order> findByUserOrderByPlacedAtDesc(User user, Pageable pageable);
 }
--- src/main/java/com/learning/springdemo/web/OrderController.java
+++ src/main/java/com/learning/springdemo/web/OrderController.java
@@ -5,6 +5,10 @@ import com.learning.springdemo.User;
 import com.learning.springdemo.data.OrderRepository;
 import com.learning.springdemo.data.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +16,7 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 import javax.validation.Valid;
@@ -20,18 +25,38 @@ import java.security.Principal;
 @Slf4j
 @Controller
 @RequestMapping("/orders")
+@SessionAttributes("order")
+//@ConfigurationProperties(prefix = "taco.orders") // section to look up in configuration file
 public class OrderController {
 
     private final OrderRepository orderRepo;
     private final UserRepository userRepo;
 
-    public OrderController(OrderRepository ordeRepo) {
+    private final OrderProps props;
+
+//    private int pageSize = 20;
+//    // This is invoked by Spring auto configuration?
+//    public void setPageSize(int pageSize) {
+//        this.pageSize = pageSize;
+//    }
+
+    public OrderController(OrderRepository ordeRepo, UserRepository userRepo, OrderProps props) {
         this.orderRepo = ordeRepo;
+        this.userRepo = userRepo;
+        this.props = props;
     }
 
     @GetMapping("/current")
     public String orderForm() {
         return "orderForm";
+    }
+
+    @GetMapping
+    public String ordersForUser(@AuthenticationPrincipal User user, Model model) {
+//        Pageable pageable = PageRequest.of(0, pageSize);
+        Pageable pageable = PageRequest.of(0, props.getPageSize());
+        model.addAttribute("orders", orderRepo.findByUserOrderByPlacedAtDesc(user, pageable));
+        return "orderList";
     }
 
     @PostMapping
