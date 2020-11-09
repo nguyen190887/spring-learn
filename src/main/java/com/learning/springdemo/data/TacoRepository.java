9234bf9130c7df4e9f516b22c022357129f4d0a4
nguyen190887 <nguyen190887@gmail.com>
2020-11-10 00:07:31 +0700

    Add new rest api

--- src/main/java/com/learning/springdemo/data/TacoRepository.java
+++ src/main/java/com/learning/springdemo/data/TacoRepository.java
@@ -1,8 +1,7 @@
 package com.learning.springdemo.data;
 
 import com.learning.springdemo.Taco;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface TacoRepository extends CrudRepository<Taco, Long> {
-    Taco save(Taco taco);
+public interface TacoRepository extends PagingAndSortingRepository<Taco, Long> {
 }
---
+++ src/main/java/com/learning/springdemo/web/RestDesignTacoController.java
@@ -0,0 +1,44 @@
+package com.learning.springdemo.web;
+
+import com.learning.springdemo.Taco;
+import com.learning.springdemo.data.TacoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.hateoas.server.EntityLinks;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/api/design", produces = "application/json")
+@CrossOrigin(origins = "*")
+public class RestDesignTacoController {
+    private TacoRepository tacoRepo;
+
+    @Autowired
+    EntityLinks entityLinks;
+
+    public RestDesignTacoController(TacoRepository tacoRepo) {
+        this.tacoRepo = tacoRepo;
+    }
+
+    //FIXME - got SQL error: Column "TACO0_.CREATED_AT" not found
+    @GetMapping("/recent")
+    public Iterable<Taco> recentTacos() {
+        PageRequest page = PageRequest.of(
+                0, 12, Sort.by(("createdAt")).descending());
+        return tacoRepo.findAll(page).getContent();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
+        Optional<Taco> optTaco = tacoRepo.findById(id);
+        if (optTaco.isPresent()) {
+            return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+}
